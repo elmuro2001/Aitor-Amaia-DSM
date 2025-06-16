7e85e7ee481d04e23a33775988b7f7c3c49ddb3b
Aitor Muro Perez <aitormuroperez@gmail.com>
2025-06-16 13:07:14 +0200

    front de creación de workplaces

--- AppCalendarioMuroVertiz/components/WorkplaceComponent.js
+++ AppCalendarioMuroVertiz/components/WorkplaceComponent.js
@@ -3,6 +3,7 @@ import { View, Text, FlatList, TouchableOpacity, Touchable } from "react-native"
 import CreacionWorkplaceModal from "../modales/CreacionWorkplace"; // Importar el modal de creación de workplac
 import AsyncStorage from "@react-native-async-storage/async-storage"; // Importar AsyncStorage para manejar el almacenamiento local
 import { Ionicons } from '@expo/vector-icons';
+import EdicionBorradoWorkplaceModal from "../modales/EdicionBorradoWorkplace";
 
 
 const WorkplaceComponent = () => {
@@ -11,6 +12,7 @@ const WorkplaceComponent = () => {
     const [workplace, setWorkplace] = useState([]); // Estado para almacenar los workplaces]);
     const [modalVisible, setModalVisible] = useState(false);//necesario para mostrar el modal de creación de workplace
     const [modalworkplace, setModalWorkplace] = useState(false); // Estado para visualizar modificaciones de un workplace
+    const [selectedWorkplace, setSelectedWorkplace] = useState(null); // Estado para almacenar el workplace seleccionado
 
     // cargar workplaces del asyncstorage
     const loadWorkplaces = async () => {
@@ -40,21 +42,31 @@ const WorkplaceComponent = () => {
                 modalVisible={modalVisible}
                 setModalVisible={setModalVisible}
             />
+            
             {/*Mostrar la lista de workplaces*/}
             <View style={{ flex: 11, flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'flex-start', padding: 10 }}>
                 <FlatList
                     data={workplace}
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({ item }) => (
-                        <TouchableOpacity style={{ paddingHorizontal: 10, borderBottomWidth: 1, borderColor: '#ccc' }} onLongPress={() => setModalWorkplace(true)}>
+                        <TouchableOpacity style={{ paddingHorizontal: 10, borderBottomWidth: 1, borderColor: '#ccc' }} onLongPress={() => { setModalWorkplace(true); setSelectedWorkplace(item); }}>
                             <Text style={{ fontSize: 18, color: item.color }}>{item.name}</Text>
                         </TouchableOpacity>
                     )}
                     horizontal={true} // Mostrar la lista horizontalmente
                     showsHorizontalScrollIndicator={false} // Ocultar la barra de desplazamiento horizontal
-
                 />
             </View>
+
+            {/* Mostrar el modal de edición y borrado de workplace solo si se ha seleccionado un workplace */}
+            {selectedWorkplace && (
+            <EdicionBorradoWorkplaceModal
+                modalworkplace={modalworkplace}
+                setModalWorkplace={setModalWorkplace}
+                selectedWorkplace={selectedWorkplace}
+                setSelectedWorkplace={setSelectedWorkplace}
+            />
+            )}
 
             {/* Botón para abrir el modal de creación de workplace */}
             <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end', padding: 10 }}>
